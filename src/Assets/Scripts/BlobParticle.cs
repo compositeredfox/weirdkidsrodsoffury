9c28ecfb557b2d3a4f128991e62d8958768d3e6d
Fernando Ramallo <fernando.ramallo@gmail.com>
2014-07-03 23:24:04 +0200

    no message

--- src/Assets/Scripts/BlobParticle.cs
+++ src/Assets/Scripts/BlobParticle.cs
@@ -1,42 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoolCat : MonoBehaviour {
+public class BlobParticle : MonoBehaviour {
 
-	public KeyCode cutoffKey;
-	public Vector2 cutoffRange = new Vector2(0.35f,0.85f);
-	public float cutoffSpeed = 0.35f;
 	public KeyCode bobKey;
-	public float bobSpeed;
+	public float shrinkSpeed = 2;
 	public AnimationCurve bobCurve = AnimationCurve.Linear(0,0,1,1);
-	float bobvalue = 1;
+	float sizeRatio = 1;
 
 	ParticleSystem ps; 
+	float originalSize = 1;
 	// Use this for initialization
 	void Start () {
-		renderer.material.mainTextureOffset = new Vector2(Random.value, Random.value);
-
 		ps = GetComponentInChildren<ParticleSystem>();
+		originalSize = ps.startSize;
 
-		renderer.material.SetFloat("_Cutoff", cutoffRange.x);
-		bobvalue = 1;
-		smr.SetBlendShapeWeight(1,bobCurve.Evaluate(bobvalue) * 100);
+		sizeRatio = 1;
+		ps.startSize = bobCurve.Evaluate(sizeRatio) * originalSize;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (renderer.material.GetFloat("_Cutoff") > cutoffRange.x)
-			renderer.material.SetFloat("_Cutoff", renderer.material.GetFloat("_Cutoff") - Time.deltaTime * cutoffSpeed);
-		if(Input.GetKeyDown(cutoffKey))
-		{
-			renderer.material.SetFloat("_Cutoff", cutoffRange.y);
-		}
 
-		if(bobvalue < 1)
-			bobvalue += Time.deltaTime*bobSpeed;
+		if(sizeRatio < 1)
+			sizeRatio += Time.deltaTime*shrinkSpeed;
 		if(Input.GetKey(bobKey))
-			bobvalue = 0;
-		smr.SetBlendShapeWeight(1,bobCurve.Evaluate(bobvalue) * 100);
-		renderer.material.mainTextureOffset = renderer.material.mainTextureOffset + Vector2.up * Time.deltaTime * 0.002f;
+			sizeRatio = 0;
+		ps.startSize = bobCurve.Evaluate(sizeRatio) * originalSize;
 	}
 }
